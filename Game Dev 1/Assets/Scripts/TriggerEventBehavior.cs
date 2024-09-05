76bbacfe6d5a86196dfdd4c73ba3d852f5e1f6ba
Max Phillips <11034905@uvu.edu>
2024-09-05 14:17:21 -0600

    backing up work

--- Game Dev 1/Assets/Scripts/TriggerEventBehavior.cs
+++ Game Dev 1/Assets/Scripts/TriggerEventBehavior.cs
@@ -3,11 +3,17 @@ using UnityEngine.Events;
 
 public class TriggerEventBehavior : MonoBehaviour
 {
-    public UnityEvent triggerEnterEvent;
+    public UnityEvent triggerEnterEvent;  // Event that will be invoked on trigger enter
+    public bool destroyOnTrigger = false; // Option to destroy the object after trigger
 
     private void OnTriggerEnter(Collider other)
     {
-        triggerEnterEvent.Invoke();
+        triggerEnterEvent.Invoke();  // Invoke the event
+
+        if (destroyOnTrigger)
+        {
+            Destroy(gameObject);  // Destroy the GameObject if the option is enabled
+        }
     }
-    
 }
+
