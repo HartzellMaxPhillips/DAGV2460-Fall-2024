0c3091d02272ce05fbcb86b9d14ecce101ddeb63
Max Phillips <11034905@uvu.edu>
2024-09-09 21:42:12 -0600

    Moved Scripts to tools submodule

--- Game Dev 1/Assets/Scripts/Vector3Data.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-//ChatGPT + documentation helped me understand this
-[CreateAssetMenu]
-public class Vector3Data : ScriptableObject
-{
-    public Vector3 value = new Vector3(1, 0, 0);  
-}
-
