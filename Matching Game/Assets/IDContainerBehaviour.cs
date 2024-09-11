b4a40633717296ab24dfc37f22963895b764e5a9
Max Phillips <11034905@uvu.edu>
2024-09-10 21:20:04 -0600

    saving changes again.

--- Matching Game/Assets/IDContainerBehaviour.cs
+++ Matching Game/Assets/IDContainerBehaviour.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class IDContainerBehaviour : MonoBehaviour
 {
     public ID idObj;
+    public UnityEvent StartEvent;
+    public void Start()
+    {
+        StartEvent.Invoke();
+    }
 }
