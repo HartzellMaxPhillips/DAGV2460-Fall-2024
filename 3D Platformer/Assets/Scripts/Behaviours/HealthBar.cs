cc3d8ff46ef9071517c110888b219907989f7447
Max Phillips <11034905@uvu.edu>
2024-11-28 01:45:53 -0700

    Checkpoints and player death implemented

--- 3D Platformer/Assets/Scripts/Behaviours/HealthBar.cs
+++ 3D Platformer/Assets/Scripts/Behaviours/HealthBar.cs
@@ -1,28 +1,35 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using System.Collections;
+
 public class HealthBar : MonoBehaviour
 {
     public Slider healthSlider;       // Reference to the slider
-    public FloatData healthData;    // Reference to the HealthData ScriptableObject
-    public UnityAction OnDeath;
-    
+    public FloatData healthData;      // Reference to the HealthData ScriptableObject
+    public UnityEvent OnDeath;        // UnityEvent for death event
+    public UnityEvent Respawn;        // UnityEvent for respawn event
+
     // Call this method whenever health changes
     public void UpdateHealthBar()
     {
         // Set the slider's value to match the current health value
         healthSlider.maxValue = healthData.maxValue;
         healthSlider.value = healthData.Value;
-        
+
         // If the health value is less than or equal to 0, call the Die method
         if (healthData.Value <= 0)
         {
-            Die();
+            OnDeath.Invoke();
+            StartCoroutine(RespawnCoroutine());
         }
     }
 
-    private void Die()
+    private IEnumerator RespawnCoroutine()
     {
-        OnDeath?.Invoke();
+        // Wait for 3 seconds
+        yield return new WaitForSeconds(3f);
+        // Invoke the Respawn event
+        Respawn.Invoke();
     }
 }
